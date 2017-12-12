6541541cf2e104b6088be5cafe5b6bb0012bff16
Simrit <simritkaur930@gmail.com>
2017-12-12 17:08:20 -0500

    Updating the examples - as proceeding with the course.

--- helloworld/truffle.js
+++ helloworld/truffle.js
@@ -4,8 +4,14 @@ module.exports = {
           host: "localhost",
           port: 8545,
           network_id: "*" // Match any network id
+    },
+    production:{
+      host: "skasix6imhh5.eastus.cloudapp.azure.com",
+      port: 8545,
+      network_id: "*",
+      gas: 4600000
     }
-}
+  }
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 };
